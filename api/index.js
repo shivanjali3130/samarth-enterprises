91bd4ac5c54d89dc9dcd0028d3a8b1aa737b9018
shivanjali3130 <walkeshivanjali970@gmail.com>
2026-06-21 18:38:13 +0530

    dd

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+// This file exists only for legacy compatibility.
+// Vercel should invoke the nested function at server/api/index.js directly.
 const app = require('../server/api/index');
-
 module.exports = app;
